7583eb354f6989d1b05dc6907e2b49d58e62087d
mighty lo <lorenz-schmid@gmx.net>
2023-04-12 12:02:50 +0200

    root-div created-App injected json scripts added

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./index.js",
+  entry: './index.js',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "main.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
-
-  target: "web",
+  target: 'web',
   devServer: {
-    port: "3000",
-    static: ["./public"],
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+},
     open: true,
     hot: true,
     liveReload: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts"],
+    extensions: ['.js', '.jsx', '.json'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        use: 'babel-loader', 
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
 };
