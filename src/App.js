0aba72181f177a58517956d1d9b16549c375fdf3
mighty lo <lorenz-schmid@gmx.net>
2023-04-13 18:19:41 +0200

    components nav routing implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import Nav from './components/Header/Nav/Nav'; 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Header/Nav/Nav'; 
+
+import Home from './components/Home/Home'; 
+import About from './components/About/About'; 
+import Projects from './components/Projects/Project'; 
+import Contact from './components/Contact/Contact'; 
 
 const App = () => {
   return (
-    <div className='App'>
-    App get's rendered? Yes Indeed!
-    <Nav />
-    </div>
-  )
-}
+    <>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}; 
 
-export default App
+export default App;
--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,27 +1,8 @@
-import React, { useState } from 'react'; 
-import styled from 'styled-components'; 
+import React from 'react'
 
 const Nav = () => {
-    const [showNav, setShowNav] = useState(false); 
-
-    const handleNavToggle = () => {
-        setShowNav(!showNav); 
-    }
-
   return (
-    <Nav>
-        <Logo>My Portfolio</Logo>
-        <Burger onClick={handleNavToggle}>
-            <MenuIcon />
-            <MenuIcon />
-            <MenuIcon />
-        </Burger>
-        <NavLinks show={showNav}>
-            <NavLink href='#'>About</NavLink>
-            <NavLink href='#'>Contact</NavLink>
-            <NavLink href='#'>Projects</NavLink>
-        </NavLinks>
-    </Nav>
+    <div>Hellouu</div>
   )
 }
 
