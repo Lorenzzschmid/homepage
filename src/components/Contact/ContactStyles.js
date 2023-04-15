7dd7b0a06b1d28eb6912546cc11c697737d22049
mighty lo <lorenz-schmid@gmx.net>
2023-04-15 22:05:08 +0200

    Form color shema

--- src/components/Contact/ContactStyles.js
+++ src/components/Contact/ContactStyles.js
@@ -11,38 +11,45 @@ export const ContactContainer = styled.div`
 export const FormContainer = styled.form`
     width: 50rem;
     height: 50rem;
-    background-color: grey;
+    background-color: #2B4061;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     justify-content: space-evenly;
-    border: 2px red dashed;
 
 `
 export const InputName = styled.input`
     width: 30rem;
     height: 3rem;
-    background-color: lightblue;
+    background-color: #AAC2E6;
     text-align: center;
+    font-size: 1.5rem;
+    border-radius: 0.5rem;
 `
 export const InputLastName = styled.input`
     width: 30rem;
     height: 3rem;
-    background-color: lightblue;
+    background-color: #AAC2E6;
     text-align: center;
-`
-export const FormSubmit = styled.button`
-    width: 30rem;
-    height: 3rem;
-    background-color: lightblue;
-    text-align: center;
+    font-size: 1.5rem;
+    border-radius: 0.5rem;
 `
 export const FormTextArea = styled.textarea`
     width: 30rem; 
     height: 10rem;
-    background-color: lightblue;
-    border-radius: 4px;
+    background-color: #AAC2E6;
+    border-radius: 0.5rem;
     text-align: center;
     justify-content: center;
-`
+    border: 2px solid black;
+    font-size: 1.5rem;
+    `
+    export const FormSubmit = styled.button`
+        width: 30rem;
+        height: 3rem;
+        background-color: #AAC2E6;
+        text-align: center;
+        font-size: 1.5rem;
+        border-radius: 0.5rem;
+    `
