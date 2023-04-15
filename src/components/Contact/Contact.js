7d027228e470bcce29beaa41799dce3fb8082237
mighty lo <lorenz-schmid@gmx.net>
2023-04-15 13:00:02 +0200

    contact form input created

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <form>
+        <label>
+          Name: 
+          <input type='text' name='name' />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
   )
 }
 
